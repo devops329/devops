f5f6b663a1991aeb52672330c367a37dc3788751
Lee <lee.jensen@agilix.com>
2024-06-10 15:28:06 -0600

    add(pizzaservice) commands and instruction for initializing data

--- instruction/jwtPizzaService/jwtPizzaService.md
+++ instruction/jwtPizzaService/jwtPizzaService.md
@@ -73,6 +73,16 @@ Here are the steps to take:
       curl http://localhost:3000/api/docs
       ```
 
+## Initial pizza data
+
+When you start up the service for the first time it will initialize your database with all of the required tables and create a single administrative user with the following defaults:
+
+- **Name**: 常用名字
+- **Email**: a@jwt.com
+- **Password**: admin
+
+You can use these credentials to populate your database with information that makes the JWT Pizza interesting to use. The easiest way to do this is to use the commands found in the [populate.sh](./populate.sh) shell script.
+
 ## Keep in sync
 
 As the application team makes changes to the frontend code you will need to sync your fork of the repository. As long as you are only adding tests and **not changing the core code**, you shouldn't have to merge any code.
---
+++ instruction/jwtPizzaService/populate.sh
@@ -0,0 +1,34 @@
+# Set the hostname - replace with your hostname if you are hitting a production system
+host=http://localhost:3000
+
+# Capture the authorization token for later use
+response=$(curl -s -X PUT $host/api/auth -d '{"email":"a@jwt.com", "password":"admin"}' -H 'Content-Type: application/json')
+token=$(echo $response | jq -r '.token')
+echo $response
+```
+
+Currently your database only has the default admin user that was inserted when the database was initialized the first time a connection was made. If you want to add some basic menu, user, franchise, and shop data you can run the following Curl commands.
+
+```sh
+# Add users
+curl -X POST $host/api/auth -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
+
+curl -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+
+
+# Add menu
+curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Pepperoni", "description": "Spicy treat", "image":"pizza2.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Margarita", "description": "Essential classic", "image":"pizza3.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Crusty", "description": "A dry mouthed favorite", "image":"pizza4.png", "price": 0.0028 }'  -H "Authorization: Bearer $token"
+
+curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Charred Leopard", "description": "For those with a darker side", "image":"pizza5.png", "price": 0.0099 }'  -H "Authorization: Bearer $token"
+
+# Add franchise
+curl -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}'  -H "Authorization: Bearer $token"
+
+# Add store
+curl -X POST $host/api/franchise/1/store -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"SLC"}'  -H "Authorization: Bearer $token"
