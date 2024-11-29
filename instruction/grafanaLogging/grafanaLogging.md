a0ca84f98114fa9298394f9af7be196b5298a1b0
James Finlinson <the30clues@gmail.com>
2024-11-28 20:11:00 -0700

    Consistently avoid ; at end of single-statement shell scripts...
    Portions of the code follow this pattern, but some portions
    had semi-colons at the end of the lines. Semi-colons are
    required in between multiple statements on the same line.
    This improves consistency across the instruction to use a
    common style.
    
    Updates were located by searching for the regex `;\s+\n`
    among only *.md and *.sh files. This produced hits in
    31 unique files, but many of these were JS code samples
    in markdown files which were manually skipped.

--- instruction/grafanaLogging/grafanaLogging.md
+++ instruction/grafanaLogging/grafanaLogging.md
@@ -108,10 +108,10 @@ for i in {1..100}; do
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $account_id:$api_key" \
     -d '{"streams": [{"stream": {"component":"jwt-pizza-service", "level": "'"$level"'", "type":"http-req"},"values": [["'"$(($(date +%s)*1000000000))"'","{\"name\":\"hacker\", \"email\":\"d@jwt.com\", \"password\":\"****\"}",{"user_id": "44","traceID": "9bc86924d069e9f8ccf09192763f1120"}]]}]}' \
-    $log_url;
+    $log_url
 
-  sleep 3;
-done;
+  sleep 3
+done
 ```
 
 This should generate enough log data to make a visualization interesting.
--- instruction/simulatingTraffic/simulatingTrafficExample/generatePizzaTraffic.sh
+++ instruction/simulatingTraffic/simulatingTrafficExample/generatePizzaTraffic.sh
@@ -20,15 +20,15 @@ trap cleanup SIGINT
 
 # Simulate a user requesting the menu every 3 seconds
 while true; do
-  curl -s "$host/api/order/menu" > /dev/null;
+  curl -s "$host/api/order/menu" > /dev/null
   echo "Requesting menu..."
   sleep 3
-done & 
+done &
 pid1=$!
 
 # Simulate a user with an invalid email and password every 25 seconds
 while true; do
-  curl -s -X PUT "$host/api/auth" -d '{"email":"unknown@jwt.com", "password":"bad"}' -H 'Content-Type: application/json' > /dev/null;
+  curl -s -X PUT "$host/api/auth" -d '{"email":"unknown@jwt.com", "password":"bad"}' -H 'Content-Type: application/json' > /dev/null
   echo "Logging in with invalid credentials..."
   sleep 25
 done &
@@ -36,27 +36,27 @@ pid2=$!
 
 # Simulate a franchisee logging in every two mintues
 while true; do
-  response=$(curl -s -X PUT $host/api/auth -d '{"email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json');
-  token=$(echo $response | jq -r '.token');
+  response=$(curl -s -X PUT $host/api/auth -d '{"email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json')
+  token=$(echo $response | jq -r '.token')
   echo "Login franchisee..."
-  sleep 110;
-  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null;
+  sleep 110
+  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null
   echo "Logging out franchisee..."
-  sleep 10;
+  sleep 10
 done &
 pid3=$!
 
 # Simulate a diner ordering a pizza every 20 seconds
 while true; do
-  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json');
-  token=$(echo $response | jq -r '.token');
+  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json')
+  token=$(echo $response | jq -r '.token')
   echo "Login diner..."
-  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d '{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}'  -H "Authorization: Bearer $token" > /dev/null;
+  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d '{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}'  -H "Authorization: Bearer $token" > /dev/null
   echo "Bought a pizza..."
-  sleep 20;
-  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null;
+  sleep 20
+  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null
   echo "Logging out diner..."
-  sleep 30;
+  sleep 30
 done &
 pid4=$!
 
