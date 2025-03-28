5cdb73cb54f7044e187b9313210f3eeed3c2032d
Ethan Wait <ethanwait25@gmail.com>
2025-03-28 10:10:29 -0600

    edit(generatePizzaTraffic) formatting

--- instruction/simulatingTraffic/simulatingTrafficExample/generatePizzaTraffic.sh
+++ instruction/simulatingTraffic/simulatingTrafficExample/generatePizzaTraffic.sh
@@ -10,65 +10,61 @@ host=$1
 
 # Function to cleanly exit
 cleanup() {
-    echo "Terminating background processes..."
-    kill $pid1 $pid2 $pid3 $pid4 $pid5
-    exit 0
+  echo "Terminating background processes..."
+  kill $pid1 $pid2 $pid3 $pid4 $pid5
+  exit 0
 }
 
 # Trap SIGINT (Ctrl+C) to execute the cleanup function
 trap cleanup SIGINT
 
 # Simulate a user requesting the menu every 3 seconds
-while true
-do
-  curl -s "$host/api/order/menu" > /dev/null;
+while true; do
+  curl -s "$host/api/order/menu" > /dev/null
   echo "Requesting menu..."
   sleep 3
-done & 
+done &
 pid1=$!
 
 # Simulate a user with an invalid email and password every 25 seconds
-while true
-do
-  curl -s -X PUT "$host/api/auth" -d '{"email":"unknown@jwt.com", "password":"bad"}' -H 'Content-Type: application/json' > /dev/null;
+while true; do
+  curl -s -X PUT "$host/api/auth" -d '{"email":"unknown@jwt.com", "password":"bad"}' -H 'Content-Type: application/json' > /dev/null
   echo "Logging in with invalid credentials..."
   sleep 25
 done &
 pid2=$!
 
-# Simulate a franchisee logging in every two mintues
-while true
-do
-  response=$(curl -s -X PUT $host/api/auth -d '{"email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json');
-  token=$(echo $response | jq -r '.token');
+# Simulate a franchisee logging in every two minutes
+while true; do
+  response=$(curl -s -X PUT $host/api/auth -d '{"email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json')
+  token=$(echo $response | jq -r '.token')
   echo "Login franchisee..."
-  sleep 110;
-  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null;
+  sleep 110
+  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null
   echo "Logging out franchisee..."
-  sleep 10;
+  sleep 10
 done &
 pid3=$!
 
 # Simulate a diner ordering a pizza every 20 seconds
-while true
-do
-  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json');
-  token=$(echo $response | jq -r '.token');
+while true; do
+  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json')
+  token=$(echo $response | jq -r '.token')
   echo "Login diner..."
-  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d '{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}'  -H "Authorization: Bearer $token" > /dev/null;
+  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d '{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}'  -H "Authorization: Bearer $token" > /dev/null
   echo "Bought a pizza..."
-  sleep 20;
-  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null;
+  sleep 20
+  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null
   echo "Logging out diner..."
-  sleep 30;
+  sleep 30
 done &
 pid4=$!
 
 # Simulate a failed pizza order every minute
-while true
-do
-  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json');
-  token=$(echo $response | jq -r '.token');
+while true; do
+  response=$(curl -s -X PUT $host/api/auth -d '{"email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json')
+  token=$(echo $response | jq -r '.token')
+  echo $response
   echo "Login hungry diner..."
 
   items='{ "menuId": 1, "description": "Veggie", "price": 0.05 }'
@@ -76,12 +72,12 @@ do
   do items+=', { "menuId": 1, "description": "Veggie", "price": 0.05 }'
   done
   
-  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d "{\"franchiseId\": 1, \"storeId\":1, \"items\":[$items]}"  -H "Authorization: Bearer $token" > /dev/null;
+  curl -s -X POST $host/api/order -H 'Content-Type: application/json' -d "{\"franchiseId\": 1, \"storeId\":1, \"items\":[$items]}"  -H "Authorization: Bearer $token"
   echo "Bought too many pizzas..."
   sleep 5;
-  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null;
+  curl -s -X DELETE $host/api/auth -H "Authorization: Bearer $token" > /dev/null
   echo "Logging out hungry diner..."
-  sleep 55;
+  sleep 55
 done &
 pid5=$!
 
